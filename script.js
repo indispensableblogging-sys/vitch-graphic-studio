c0fedc88407d34e10eb1cddd1a8c92a1d507d063
indispensableblogging-sys <indispensableblogging@gmail.com>
2026-07-26 20:59:56 +0100

    Add scroll reveal effect to elements

--- script.js
+++ script.js
@@ -31,3 +31,18 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll(){
+    reveals.forEach(item=>{
+        const windowHeight = window.innerHeight;
+        const elementTop = item.getBoundingClientRect().top;
+
+        if(elementTop < windowHeight - 100){
+            item.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
