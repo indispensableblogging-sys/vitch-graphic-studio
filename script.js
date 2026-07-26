62e2c772e28d91c8787b45e68148a641727e4bf8
indispensableblogging-sys <indispensableblogging@gmail.com>
2026-07-26 20:10:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const navbar = document.getElementById("navbar");
 menuBtn.addEventListener("click", function () {
     navbar.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        navbar.classList.remove("active");
+    });
+});
