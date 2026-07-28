eddc3a43781ebcb4623234c8cd7898a6fc03e823
indispensableblogging-sys <indispensableblogging@gmail.com>
2026-07-28 18:03:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const navbar = document.getElementById("navbar");
 menuToggle.addEventListener("click", () => {
     navbar.classList.toggle("active");
 });
+const reveals = document.querySelectorAll(".reveal");
+
+function revealSections() {
+    reveals.forEach((section) => {
+        const windowHeight = window.innerHeight;
+        const sectionTop = section.getBoundingClientRect().top;
+        const revealPoint = 100;
+
+        if (sectionTop < windowHeight - revealPoint) {
+            section.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealSections);
+window.addEventListener("load", revealSections);
