193ae63de4f4c5ca4869120a6cc3e3a53d84d682
indispensableblogging-sys <indispensableblogging@gmail.com>
2026-07-30 07:29:29 +0100

    Implement currency change event for price updates
    Added event listener to update prices based on currency selection.

--- script.js
+++ script.js
@@ -20,3 +20,33 @@ function revealSections() {
 
 window.addEventListener("scroll", revealSections);
 window.addEventListener("load", revealSections);
+const currency = document.getElementById("currency");
+const prices = document.querySelectorAll(".price");
+
+currency.addEventListener("change", function () {
+
+    if (this.value === "NGN") {
+        prices[0].innerHTML = "₦5,000";
+        prices[1].innerHTML = "₦15,000";
+        prices[2].innerHTML = "₦35,000";
+    }
+
+    if (this.value === "USD") {
+        prices[0].innerHTML = "$10";
+        prices[1].innerHTML = "$25";
+        prices[2].innerHTML = "$60";
+    }
+
+    if (this.value === "SLL") {
+        prices[0].innerHTML = "Le 230";
+        prices[1].innerHTML = "Le 690";
+        prices[2].innerHTML = "Le 1,610";
+    }
+
+    if (this.value === "GHS") {
+        prices[0].innerHTML = "GH₵105";
+        prices[1].innerHTML = "GH₵260";
+        prices[2].innerHTML = "GH₵620";
+    }
+
+});
