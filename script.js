e8af103db058772434b681d5bac9c68de95ab872
indispensableblogging-sys <indispensableblogging@gmail.com>
2026-08-02 20:20:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -145,3 +145,17 @@ counters.forEach(counter => {
     updateCounter();
 
 });
+// Luxury Preloader
+
+window.addEventListener("load", () => {
+
+    const preloader = document.getElementById("preloader");
+
+    if (preloader) {
+        setTimeout(() => {
+            preloader.style.opacity = "0";
+            preloader.style.visibility = "hidden";
+        }, 1800);
+    }
+
+});
